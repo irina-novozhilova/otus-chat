6ca1d61bc9a7211d17c15220a30110e584be50f9
Irina Novozhilova <irina@nsvision.com>
2023-06-12 21:29:50 +0400

    add chat

--- config/__mocks__/dom.ts
+++ config/__mocks__/dom.ts
@@ -1,6 +1,7 @@
 import { JSDOM } from "jsdom";
 
 const dom = new JSDOM();
-global.document = dom.window.document;
 // @ts-ignore
-global.window = dom.window;
+global.document ??= dom.window.document;
+// @ts-ignore
+global.window ??= dom.window;
